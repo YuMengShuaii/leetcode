49a03ef3fbde5ba56fb51f340d92bd78ab8fa354
yumengshuai <yumengshuai@lansent.com>
2019-12-19 11:43:04 +0800

    init

---
+++ src/main/java/com/kawo/algorithm/code/weeka/impl/WeekAHomeWork.kt
@@ -0,0 +1,91 @@
+package com.kawo.algorithm.code.weeka.impl
+
+import com.kawo.algorithm.code.weeka.agreement.WeekAHomeWorkAgreement
+import java.util.*
+
+class WeekAHomeWork : WeekAHomeWorkAgreement {
+
+    override fun maxArea(height: IntArray): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun moveZeroes(nums: IntArray) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun climbStairs(n: Int): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun threeSum(nums: IntArray): List<List<Int>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <NodeType> reverseKGroup(head: NodeType, k: Int): NodeType {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <NodeType> detectCycle(head: NodeType): NodeType {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <NodeType> hasCycle(head: NodeType): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <NodeType> swapPairs(head: NodeType): NodeType {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <NodeType> reverseList(head: NodeType): NodeType {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun plusOne(digits: IntArray): IntArray {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun twoSum(nums: IntArray, target: Int): IntArray {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <NodeType> mergeTwoLists(l1: NodeType, l2: NodeType): NodeType {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun rotate(nums: IntArray, k: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun removeDuplicates(nums: IntArray): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun trap(height: IntArray): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <ValueType, DequeType : Deque<ValueType>> dequeTest(deque: DequeType) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun largestRectangleArea(heights: IntArray): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun minStack() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isValid(s: String): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
